6dcbbd886fd63aa95c2bf1eafd9b919c243b68db
Chaerul Umam <chaerulumamb244@gmail.com>
2023-03-16 20:37:03 +0700

    authentication register

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,5 @@
 import Input from "@/components/Input";
+import axios from "axios";
 import React, { useCallback, useState } from "react";
 
 const Auth = () => {
@@ -13,6 +14,18 @@ const Auth = () => {
       currentVariant === "login" ? "register" : "login"
     );
   }, []);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password]);
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -50,7 +63,10 @@ const Auth = () => {
                 onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button
+              onClick={register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
             <p className="text-neutral-500 mt-12">
