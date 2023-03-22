45682e3db30f40dd537ac22a6f8cefde8fc9e4e6
Chaerul Umam <chaerulumamb244@gmail.com>
2023-03-22 13:29:02 +0700

    login section

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth/next";
 import Credentials from "next-auth/providers/credentials";
 import { compare } from "bcrypt";
 
-import prismadb from "../../lib/prisma";
+import prismadb from "../../../lib/prisma";
 
 export default NextAuth({
   providers: [
